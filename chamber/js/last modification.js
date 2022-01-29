cc64814c6cd0570bcc6a723e7f9892cce907997a
gansukhBOLORSAIKHAN <gan19008@byui.edu>
2022-01-29 17:08:09 +0800

    qwqw

--- chamber/js/last modification.js
+++ chamber/js/last modification.js
@@ -1,6 +1,10 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-}
+const hamButton = document.querySelector('.ham');
+const mainMenu = document.querySelector('.navigation');
 
-const x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
+hamButton.addEventListener('click', () => { mainMenu.classList.toggle('responsive') }, false);
+
+window.onresize = () => {
+    if (window.innerWidth > 760) {
+        mainMenu.classList.remove('responsive');
+    }
+}

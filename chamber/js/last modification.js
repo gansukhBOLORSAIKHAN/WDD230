9af8be12308d98c97f0506641b4225511c99a9dd
gansukhBOLORSAIKHAN <gan19008@byui.edu>
2022-01-29 17:21:32 +0800

    huulav

--- chamber/js/last modification.js
+++ chamber/js/last modification.js
@@ -8,3 +8,8 @@ window.onresize = () => {
         mainMenu.classList.remove('responsive');
     }
 }
+var currentDate = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("output").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();

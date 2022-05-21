8d132817db09600b814259fb96cf81a9dd566960
gansukhBOLORSAIKHAN <rolltowel@gmail.com>
2022-05-22 06:39:25 +0800

    asas

--- week5/js/script.js
+++ week5/js/script.js
@@ -1,5 +1,3 @@
-document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: " + document.lastModified
-
 function toggleMenu() {
     var temp = document.getElementsByClassName("toggler")[0]
     temp.classList.toggle("hide")
